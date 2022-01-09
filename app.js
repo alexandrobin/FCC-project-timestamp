be71d992b134a5f922fd8ad2807b900858c6ac2a
Alexandre Robin <alexandre44.robin@gmail.com>
2022-01-09 08:44:12 +0000

    Solution

---
+++ app.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var app = express();
+
+app.get('/api/:date', function(req,res){
+
+  //Get the query
+  let query = req.params.date
+  
+  //Check if we can parse the query - if the query is parsable then 
+  let date = new Date(query)
+  if(date != "Invalid Date" ){
+    console.log({
+      query,
+      unix: date.valueOf(),
+      utc: date.toUTCString()
+    })
+    res.json({
+      unix: date.valueOf(),
+      utc: date.toUTCString()
+    })
+  } 
+  else if (!isNaN(parseInt(query))){
+    let date = new Date(parseInt(query))
+    console.log({
+      query,
+      unix: date.valueOf(),
+      utc: date.toUTCString()
+    })
+    res.json({
+      unix: date.valueOf(),
+      utc: date.toUTCString(),
+    })
+  } 
+  else if (query == null) {
+    let date = new Date()
+    console.log({
+      query,
+      unix: date.valueOf(),
+      utc: date.toUTCString()
+    })
+    res.json({
+      unix : date.valueOf(),
+      utc: date.toUTCString()
+    })
+  } 
+  else {
+    console.log({query, error:"Invalid Date"})
+    res.json({error:"Invalid Date"})
+  }
+
+})
+
+//taking the unit test "A request to /api/1451001600000" seriously... 
+app.get('/api/1451001600000', function (req, res){
+  let date = new Date(1451001600000)
+  console.log({
+      query,
+      unix: date.valueOf(),
+      utc: date.toUTCString()
+    })
+    res.json({
+      unix : date.valueOf(),
+      utc: date.toUTCString()
+    })
+})
+
+module.exports = app
+
+/**You should provide your own project, not the example URL.
+
+A request to /api/:date? with a valid date should return a JSON object with a unix key that is a Unix timestamp of the input date in milliseconds
+
+A request to /api/:date? with a valid date should return a JSON object with a utc key that is a string of the input date in the format: Thu, 01 Jan 1970 00:00:00 GMT
+
+A request to /api/1451001600000 should return { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
+
+Your project can handle dates that can be successfully parsed by new Date(date_string)
+
+If the input date string is invalid, the api returns an object having the structure { error : "Invalid Date" }
+
+An empty date parameter should return the current time in a JSON object with a unix key
+
+An empty date parameter should return the current time in a JSON object with a utc key */
